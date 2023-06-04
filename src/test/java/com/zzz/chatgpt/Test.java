08d4a157925b0b693774f72a19e6c48c5bec5bfc
zpy <873333508@qq.com>
2023-06-04 17:14:01 +0800

    init

---
+++ src/test/java/com/zzz/chatgpt/Test.java
@@ -0,0 +1,63 @@
+package com.zzz.chatgpt;
+
+import com.zzz.chatgpt.entity.chat.ChatCompletion;
+import com.zzz.chatgpt.entity.chat.ChatCompletionResponse;
+import com.zzz.chatgpt.entity.chat.Message;
+import com.zzz.chatgpt.util.Proxys;
+
+import org.junit.Before;
+
+import java.net.Proxy;
+import java.util.Arrays;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("test");
+    }
+
+    private ChatGPT chatGPT;
+
+    @Before
+    public void before() {
+        Proxy proxy = Proxys.http("127.0.0.1", 10809);
+
+        chatGPT = ChatGPT.builder()
+                .apiKey("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")
+                .timeout(900)
+                .proxy(proxy)
+                .apiHost("https://api.openai.com/") //代理地址
+                .build()
+                .init();
+
+//        CreditGrantsResponse response = chatGPT.creditGrants();
+//        log.info("余额：{}", response.getTotalAvailable());
+    }
+
+    @org.junit.Test
+    public void chat() {
+        Message system = Message.ofSystem("你现在是一个诗人，专门写七言绝句");
+        Message message = Message.of("写一段七言绝句诗，题目是：火锅！");
+
+        ChatCompletion chatCompletion = ChatCompletion.builder()
+                .model(ChatCompletion.Model.GPT_3_5_TURBO.getName())
+                .messages(Arrays.asList(system, message))
+                .maxTokens(3000)
+                .temperature(0.9)
+                .build();
+        ChatCompletionResponse response = chatGPT.chatCompletion(chatCompletion);
+        Message res = response.getChoices().get(0).getMessage();
+        System.out.println(res);
+    }
+
+    @org.junit.Test
+    public void chatmsg() {
+        String res = chatGPT.chat("写一段七言绝句诗，题目是：火锅！");
+        System.out.println(res);
+    }
+
+}
